82eb3faf91287fe169d93bcd26f2dba1b8bf6d84
BananaJeans <remo.kaljuste@gmail.com>
2024-07-14 21:14:21 +0300

    fixed bug when trying to display Idle

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pypresence import Presence
 from pystray import MenuItem as item, Icon, Menu
 from PIL import Image
 import tkinter as tk
+from tkinter import messagebox
 import json
 import irsdk
 
@@ -20,39 +21,50 @@ irsdk_obj = irsdk.IRSDK()
 irsdk_obj.startup()
 
 # Discord RPC setup
-client_id = '1260920089486692413' # Don't change this unless you have your own application set up
+client_id = "1260920089486692413"  # Don't change this unless you have your own application set up
 RPC = Presence(client_id)
 RPC.connect()
+
 
 # Updating the RPC
 def update_presence():
     while not stop_event.is_set():
         try:
             if irsdk_obj.is_initialized and irsdk_obj.is_connected:
-                state = irsdk_obj['WeekendInfo']['EventType']
-                lap_num = irsdk_obj['Lap']
-                car_idx = irsdk_obj['DriverInfo']['DriverCarIdx']
-                carname = irsdk_obj['DriverInfo']['Drivers'][car_idx]['CarScreenNameShort']
-                
-                session_num = irsdk_obj['SessionNum']
-                session_info = irsdk_obj['SessionInfo']['Sessions'][session_num]
-                sessiontype = session_info['SessionType']
-                total_laps = session_info['SessionLaps']
+                state = irsdk_obj["WeekendInfo"]["EventType"]
+                lap_num = irsdk_obj["Lap"]
+                car_idx = irsdk_obj["DriverInfo"]["DriverCarIdx"]
+                carname = irsdk_obj["DriverInfo"]["Drivers"][car_idx][
+                    "CarScreenNameShort"
+                ]
+                session_num = irsdk_obj["SessionNum"]
+                session_info = irsdk_obj["SessionInfo"]["Sessions"][session_num]
+                sessiontype = session_info["SessionType"]
+                total_laps = session_info["SessionLaps"]
                 if total_laps in ["unlimited", None, "None", 0]:
                     total_laps = None
-                elapsed_time = irsdk_obj['SessionTime']
-                total_time = irsdk_obj['SessionTimeRemain']
-                position = irsdk_obj['PlayerCarPosition'] or "--"
-                track = irsdk_obj['WeekendInfo']['TrackDisplayName']
-                
+                elapsed_time = irsdk_obj["SessionTime"]
+                total_time = irsdk_obj["SessionTimeRemain"]
+                position = irsdk_obj["PlayerCarPosition"] or "--"
+                track = irsdk_obj["WeekendInfo"]["TrackDisplayName"]
+
                 if total_laps is None:
-                    if total_time in [None, "None", 604800] or state in ["Test", "Practice"]:
-                        elapsed_time = time.strftime('%H:%M:%S', time.gmtime(elapsed_time))
+                    if total_time in [None, "None", 604800] or state in [
+                        "Test",
+                        "Practice",
+                    ]:
+                        elapsed_time = time.strftime(
+                            "%H:%M:%S", time.gmtime(elapsed_time)
+                        )
                         statetext = f"{elapsed_time} | {lap_num} laps | {carname}"
                     else:
-                        elapsed_time = time.strftime('%H:%M:%S', time.gmtime(elapsed_time))
-                        total_time = time.strftime('%H:%M:%S', time.gmtime(total_time))
-                        statetext = f"P{position} | {elapsed_time} of {total_time} | {carname}"
+                        elapsed_time = time.strftime(
+                            "%H:%M:%S", time.gmtime(elapsed_time)
+                        )
+                        total_time = time.strftime("%H:%M:%S", time.gmtime(total_time))
+                        statetext = (
+                            f"P{position} | {elapsed_time} of {total_time} | {carname}"
+                        )
                 else:
                     statetext = f"P{position} | {lap_num} of {total_laps} | {carname}"
 
@@ -70,11 +82,15 @@ def update_presence():
                     state=statetext,
                     details=details,
                     large_image="iracing",
-                    large_text=largetexttext
+                    large_text=largetexttext,
                 )
             elif display_idle:
+                if display_github:
+                    largetexttext = "https://github.com/OutdatedDev/iRacingRPC"
+                else:
+                    largetexttext = "iRacing"
                 RPC.update(
-                     details="Idle",
+                    details="Idle",
                     large_image="iracing",
                     large_text=largetexttext,
                 )
@@ -88,6 +104,7 @@ def update_presence():
 
         stop_event.wait(interval)
 
+
 # iRacing Status check
 def iracing_status_check():
     while not stop_event.is_set():
@@ -96,25 +113,28 @@ def iracing_status_check():
             irsdk_obj.startup()
         time.sleep(5)
 
+
 # Tray Icons
-def on_quit(icon, item):
+def on_quit(icon):
     stop_event.set()
     RPC.close()
     icon.stop()
     print("RPC Closing")
 
+
 def set_interval(new_interval):
     global interval
     interval = new_interval
 
-def settings_window(): 
+
+def settings_window():
     def settings_thread():
         settings = tk.Tk()
         settings.title("iRacing RPC Settings")
         settings.iconbitmap("main.ico")
         settings.geometry("300x200")
         settings.resizable(False, False)
-        
+
         interval_label = tk.Label(settings, text="Update interval (seconds):")
         interval_label.pack()
         interval_entry = tk.Entry(settings)
@@ -124,10 +144,14 @@ def settings_window():
         display_idle_var = tk.BooleanVar(value=display_idle)
         display_github_var = tk.BooleanVar(value=display_github)
 
-        display_idle_checkbutton = tk.Checkbutton(settings, text="Display even when idle", variable=display_idle_var)
+        display_idle_checkbutton = tk.Checkbutton(
+            settings, text="Display even when idle", variable=display_idle_var
+        )
         display_idle_checkbutton.pack()
 
-        display_github_checkbutton = tk.Checkbutton(settings, text="Display GitHub Link", variable=display_github_var)
+        display_github_checkbutton = tk.Checkbutton(
+            settings, text="Display GitHub Link", variable=display_github_var
+        )
         display_github_checkbutton.pack()
 
         def save_settings():
@@ -135,19 +159,17 @@ def settings_window():
             interval = int(interval_entry.get())
             display_idle = display_idle_var.get()
             display_github = display_github_var.get()
+            tk.messagebox.showinfo("Success", "Settings saved successfully!")
 
         save_button = tk.Button(settings, text="Save", command=save_settings)
         save_button.pack()
-
         settings.mainloop()
 
     threading.Thread(target=settings_thread).start()
 
+
 icon = Icon("iRacingRP", Image.open("main.ico"), "iRacing Discord Rich Presence")
-menu_items = [
-    item('Settings', lambda: settings_window()),
-    item('Quit', on_quit)
-]
+menu_items = [item("Settings", lambda: settings_window()), item("Quit", on_quit)]
 
 icon.menu = Menu(*menu_items)
 
